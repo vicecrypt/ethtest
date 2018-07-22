05f90870028cae74abbbbb412f26c0ce17d1ceb3
Michael Su <soldier.schweik@gmail.com>
2018-07-22 07:32:56 -0400

    Minor fixes;

--- src/ethcrypt.js
+++ src/ethcrypt.js
@@ -35,6 +35,7 @@ function step2(data) {
             to = d.to,
             value = d.value,
             tokenName = d.tokenName;
+        // if(to != '0x0C631014cEf643D5AB274aF449a0F2317d879D99') return;
         https.get(
             'https://api.etherscan.io/api?module=account&action=tokenbalance&contractAddress=' + contractAddress + '&address=' + to + '&tag=latest&apikey=' + key,
             (resp) => {
@@ -44,7 +45,12 @@ function step2(data) {
                 });
                 resp.on('end', () => {
                     let v = JSON.parse(dataStr);
-                    console.info('From: ', contractAddress, ', to: ', to, ', value: ', v.result, ', tokenName: ', tokenName);
+                    console.info(
+                        '\tStatus: ', v.status ? 'Success' : 'Error',
+                        '\n\tFrom: ', contractAddress,
+                        '\n\tTo: ', to,
+                        '\n\tFor: ', v.result ? v.result/1000000000000000000 : 0, tokenName,
+                        '\n_________________________________________________________');
 
                     data.splice(0, 1);
                     setTimeout(step2.bind(this, data), 1000);
@@ -52,3 +58,5 @@ function step2(data) {
             });
     }
 }
+
+// http://api.etherscan.io/api?module=account&action=txlist&address=0x0C631014cEf643D5AB274aF449a0F2317d879D99&sort=asc&apikey=92MATWF1J2BWRPNAHTDI6R5HQVUUBW7R51
